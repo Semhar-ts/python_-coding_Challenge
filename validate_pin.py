4f42d62d8ea15f7aee738e96d528628875c36b9f
Semhar-ts <77295597+Semhar-ts@users.noreply.github.com>
2021-06-14 14:25:24 +0200

    Add files via upload

--- validate_pin.py
+++ validate_pin.py
@@ -1,1 +1,12 @@
 
+def validate_pin(pin):
+    return len(pin)in (4, 6) and pin.isdigit()
+
+print ('validating pin.')
+print ('pin are 4 digit or 6 digit , and only number .')
+print ( " ex.pin = '1234' or pin = '123456' >> True ")
+print ( " ex.pin = '123' or pin = '12345a' >> False \n ")
+
+pin = input('Enter pin:')
+print (" the result :")
+print (validate_pin(pin))
